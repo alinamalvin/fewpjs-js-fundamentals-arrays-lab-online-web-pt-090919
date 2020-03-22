69d0305d5df93a833107bdddd65e25e65ebe3f09
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 22:47:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prepend=["Milo", "Otis", "Garfield"]
 prepend.unshift("Odie")
 
 const removeLast=["Milo", "Otis", "Garfield"]
-removeLast.pop("Odie")
+removeLast.pop()
 
 const append=["Milo", "Otis", "Garfield"]
 append.push("Odie")
